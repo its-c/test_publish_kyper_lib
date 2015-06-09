9e6d55d8aab2dbc4c4cddbca8330e411faff97ce
its-c <quits.c@gmail.com>
2015-06-09 16:18:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'test_publish',
+  packages = ['test_publish'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A test for publishing kyper library',
+  author = 'ITzu Chen',
+  author_email = 'itzu.chen@kyperdata.com',
+  url = '', # use the URL to the github repo
+  download_url = '', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
